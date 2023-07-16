8b9bf86fcb97540f648eafc3989b8d199b8780e0
laeo <8142123+laeo@users.noreply.github.com>
2023-07-16 15:58:17 +0800

    fix: hide disposable option

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,11 +19,11 @@
         <input type="text" name="edit" placeholder="Edit Password" value="{{ old('edit') }}" id="edit" class=" outline outline-slate-200 p-2 rounded bg-white" autocomplete="off" minlength="1" maxlength="64">
         <input type="text" name="access" placeholder="Access Password" value="{{ old('access') }}" id="access" class=" outline outline-slate-200 p-2 rounded bg-white" autocomplete="off" minlength="1" maxlength="64">
 
-        <label class="relative inline-flex items-center cursor-pointer select-none">
+        <!-- <label class="relative inline-flex items-center cursor-pointer select-none">
             <input type="checkbox" name="onetime" value="1" @checked(old('onetime')) class="sr-only peer">
             <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
             <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 uppercase">Burn after read</span>
-        </label>
+        </label> -->
 
         <button onclick="submitForm()" class=" min-w-[8rem] py-2 rounded bg-zinc-950 text-white">Go</button>
     </form>
