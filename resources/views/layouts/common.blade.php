c10c50f854ddb25c0fcd7f02689301b42d8e0bc8
laeo <8142123+laeo@users.noreply.github.com>
2023-07-16 16:49:47 +0800

    fix: site name

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'text.is')</title>
+    <title>@yield('title', config('app.name'))</title>
 
     @stack('styles')
     @vite('resources/css/app.css')
@@ -12,7 +12,7 @@
 
 <body class=" max-w-screen-lg mx-auto bg-slate-50 p-5 h-screen flex flex-col gap-8">
     <nav class=" w-full bg-white mx-auto p-4 rounded select-none flex justify-between">
-        <div class=" uppercase font-bold">text.is</div>
+        <div class=" uppercase font-bold">{{ config('app.name') }}</div>
         <div class=" flex gap-4 text-base text-slate-600 transition-colors">
             @foreach ($links as $link)
             <a target="{{ $link->target }}" class=" hover:text-slate-900" href="{{ $link->url }}">{{ $link->title }}</a>
@@ -23,7 +23,7 @@
     @yield('content')
 
     <footer class=" flex flex-col justify-center items-center">
-        <div class=" text-slate-800">&copy; text.is 2023</div>
+        <div class=" text-slate-800">&copy; {{ config('app.name') }} 2023</div>
     </footer>
 
     @stack('scripts')
